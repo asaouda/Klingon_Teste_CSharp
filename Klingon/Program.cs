50fa073bab3ea3103467e5e45874fc938e5a6a11
asaouda <alinesaouda@gmail.com>
2020-06-22 23:17:51 -0300

    Correções

--- Klingon/Program.cs
+++ Klingon/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Klingon
+namespace Klingon_Desafio
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            
+
+
         }
+
     }
 }
